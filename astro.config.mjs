dda58d81902f05221a6f0719b521c1c174de77dd
everssteeve <48457784+everssteeve@users.noreply.github.com>
2026-03-26 19:27:55 +0100

    fix: add trailing slash to base in astro.config.mjs for correct BASE_URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-    site: 'https://everssteeve.github.io',
-    base: '/AIAD-formation',
-    integrations: [mdx()],
+  site: 'https://everssteeve.github.io',
+  base: '/AIAD-formation/',
+  integrations: [mdx()],
 });
